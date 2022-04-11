ce7d1096b307761ee2278b6f81d14370cad05642
LeooZeballos <zeballosleonel3@gmail.com>
2022-04-11 15:09:15 -0300

    added Lombok to Product + javadocs

--- src/main/java/net/leozeballos/FastFood/product/Product.java
+++ src/main/java/net/leozeballos/FastFood/product/Product.java
@@ -1,76 +1,69 @@
 package net.leozeballos.FastFood.product;
 
+import lombok.*;
 import net.leozeballos.FastFood.item.Item;
 import net.leozeballos.FastFood.menu.Menu;
+import org.hibernate.Hibernate;
 
 import javax.persistence.*;
 import java.util.Collection;
+import java.util.Objects;
 
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Product implements Item {
 
+    /**
+     * The unique identifier of the product.
+     */
     @Id
     @GeneratedValue(strategy = javax.persistence.GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * The name of the product.
+     */
     @Column(nullable = false, length = 50)
     private String name;
 
+    /**
+     * The price of the product.
+     */
     @Column(nullable = false)
     private double price;
 
-    @ManyToMany(mappedBy = "products")
-    private Collection<Menu> menus;
-
-    public Product() {
-    }
-
-    public Product(String name, double price) {
-        this.name = name;
-        this.price = price;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Collection<Menu> getMenus() {
-        return menus;
-    }
-
-    public void setMenus(Collection<Menu> menus) {
-        this.menus = menus;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Product product = (Product) o;
+        return id != null && Objects.equals(id, product.id);
     }
 
     @Override
-    public String toString() {
-        return "Product [name=" + name + ", price=" + price + ", id=" + id + "]";
+    public int hashCode() {
+        return getClass().hashCode();
     }
 
+    /**
+     * Item's interface implementation
+     * @return double Product's price
+     */
+    @Override
     public double calculatePrice() {
         return price;
     }
 
+    /**
+     * Formats the Product's price. Example: $1.00
+     * @return String Product's price formatted
+     */
     public String getFormattedPrice() {
         return "$" + String.format("%.2f", price);
     }
