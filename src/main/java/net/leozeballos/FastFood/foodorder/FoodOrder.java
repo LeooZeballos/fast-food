3e4ea8a7fa0a719d6f778fb69242fdd4e5f86355
LeooZeballos <zeballosleonel3@gmail.com>
2022-04-11 15:12:34 -0300

    foodorder javadoc

--- src/main/java/net/leozeballos/FastFood/foodorder/FoodOrder.java
+++ src/main/java/net/leozeballos/FastFood/foodorder/FoodOrder.java
@@ -18,19 +18,34 @@ import java.util.Objects;
 @Entity
 public class FoodOrder {
 
+    /**
+     * The unique identifier of the food order.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * The creation date and time of the food order.
+     */
     @Column(nullable = false)
     private final java.sql.Timestamp creationTimestamp = java.sql.Timestamp.valueOf(LocalDateTime.now());
 
+    /**
+     * The payment date and time of the food order.
+     */
     @Column
     private java.sql.Timestamp paymentTimestamp;
 
+    /**
+     * The status of the food order.
+     */
     @Enumerated(EnumType.STRING)
     private FoodOrderState state;
 
+    /**
+     * The branch that the food order is for.
+     */
     @ManyToOne(optional = false)
     @JoinColumn(name = "branch_id", nullable = false)
     private Branch branch;
@@ -47,4 +62,5 @@ public class FoodOrder {
     public int hashCode() {
         return getClass().hashCode();
     }
+
 }
